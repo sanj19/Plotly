a7aeb444c9107f3d1909d1966f41b5c46d8394f2
Sanjiv Patel <sanj19@yahoo.com>
2020-01-09 09:28:13 -0500

    in progress

---
+++ app.js
@@ -0,0 +1,21 @@
+//use D3 to fetch the json file
+d3.json("samples.json").then((importedData) => {
+    console.log(importedData);
+    var data = importedData
+    
+    data.sort(function(a, b) {
+        return 
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+});
